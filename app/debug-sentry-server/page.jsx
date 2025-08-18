07e86fad401609b7cd7e9cd9b6b4a963d2209a82
rajiv-creator <rajiv.growindia11@gmail.com>
2025-08-18 14:54:13 +0530

    Delete app/debug-sentry-server/page.jsx

--- app/debug-sentry-server/page.jsx
+++
@@ -1,6 +0,0 @@
-export const dynamic = 'force-dynamic'; // prevents prerendering at build
-
-export default function DebugSentryServer() {
-  // This runs on the server at request time and will throw intentionally.
-  throw new Error('Server blew up on purpose!');
-}
